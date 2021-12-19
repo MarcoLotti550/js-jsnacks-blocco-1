004c8f5711fc340484eb6b375fb780a1dcd73a52
MarcoLotti <gorlandelli64@gmail.com>
2021-12-19 15:56:55 +0100

    snack2 e 3

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,12 @@
 Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 Ogni volta che ne crei uno, stampalo a schermo. */
 
+let n = parseInt(prompt("Inserisci un numero"));
+
+for (let i = 0; i < n; i++){
+  let numeriInseriti = [];
+  for (let i2 = 0; i2 < 10; i2++){
+    numeriInseriti.push(Math.floor(Math.random() * 100) +1);
+  };
+  document.writeln("<div>" + numeriInseriti + "</div>");
+};
