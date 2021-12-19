3bbc5aa26e1120099d5af3bac0feb1e0ca5f308c
MarcoLotti <gorlandelli64@gmail.com>
2021-12-19 14:29:01 +0100

    primo jsnck

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,16 @@
 Continua a chiedere i numeri all’utente e a inserirli nell’array fino 
 a quando la somma degli elementi è minore di 50. */
 
+let numeriInseriti = []
+let i = 0;
+let sum = 0;
+
+do {
+  let n = parseInt(prompt("Inserisci un numero"));
+  numeriInseriti.push(n);
+  sum += numeriInseriti[i];
+  i++;
+}
+while (sum < 50);
+
+console.log(sum);
