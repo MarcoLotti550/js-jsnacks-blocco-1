42baedbcc23fbe1cf9fe20ed69f5624a43fd22c7
MarcoLotti <gorlandelli64@gmail.com>
2021-12-19 15:57:16 +0100

    snack 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,4 +3,20 @@ un div avrà il testo colorato di rosso mentre l’altro di verde.
 Partendo da un array di numeri, stampiamo nell’id rosso i numeri
  dispari e in verde i numeri pari. */
 
+ let divRed = document.createElement("div");
+ divRed.setAttribute("id", "red");
+ document.body.appendChild(divRed);
+
+ let divGreen = document.createElement("div");
+ divGreen.setAttribute("id", "green");
+ document.body.appendChild(divGreen);
  
+ let lostNumbers = [4, 8, 15, 16, 23, 42];
+ 
+ for (i = 0; i < lostNumbers.length; i++){
+   if (lostNumbers[i] % 2 != 0){
+     divRed.innerHTML += lostNumbers[i] + " ";
+   }else {
+     divGreen.innerHTML += lostNumbers[i] + " ";
+   };
+ };
